f93a2d3eb186079462ca41d9916fb980616099b2
Ricardo Rodriguez Gonzalez <rrodriguez@agilistik.cl>
2021-05-08 14:57:06 -0400

    Clase 16-18

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../assets/styles/components/Heades.scss';
 
 const Header = () => (
     <header className="header">
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import Header from "../componets/Header";
+import '../assets/styles/App.scss'
+import Search from "../componets/Search";
+import Categories from "../componets/Categories";
+import Carousel from "../componets/Carousel";
+import CarouselItem from "../componets/CarouselItem";
 
 const App = () => (
     <div className="App">
         <Header/>
+        <Search/>
+        <Categories>
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
     </div>
 );
 export default App;
