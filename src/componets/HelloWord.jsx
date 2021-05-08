6e68e65d0af8753e4941f36ec77e2127afc71b6d
Ricardo Rodriguez Gonzalez <rrodriguez@agilistik.cl>
2021-05-08 13:46:50 -0400

    hasta aqui todo bien, clase 12

--- src/componets/HelloWord.jsx
+++ src/componets/HelloWord.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../assets/styles/App.scss';
 
 const HelloWord = () => (
-    <h1>Hola mundo</h1>
+    <h1>Hola mundo de Ricardo</h1>
 );
 
 export default HelloWord;
