51f15fe41ab4aa57eb6e125c1f78bf9aee055d92
Ricardo Rodriguez Gonzalez <rrodriguez@agilistik.cl>
2021-05-09 14:24:55 -0400

    Curso 2: Clase 7

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -14,8 +14,7 @@ const API = 'http://localhost:3000/initialState';
 const Home = () => {
     const initialState = useTvShowsApi(API);
     return initialState.length === 0 ? <h1>Loading...</h1> : (
-        <div className="App">
-            <Header/>
+        <>
             <Search/>
             {initialState.mylist.length > 0 &&
             <Categories title="Mi lista">
@@ -34,8 +33,7 @@ const Home = () => {
                     {initialState.originals.map(item => <CarouselItem key={item.id} {...item}/>)}
                 </Carousel>
             </Categories>
-            <Footer/>
-        </div>
+        </>
     );
 }
 export default Home;
