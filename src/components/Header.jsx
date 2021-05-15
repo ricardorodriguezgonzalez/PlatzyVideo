ae2c2f612d88f710b8158c6f1d32e5c75c8b079d
Ricardo Rodriguez Gonzalez <rrodriguez@agilistik.cl>
2021-05-15 15:58:14 -0400

    Curso 2: Clase 18-19 Manejo de Iniciar seccion y gravatar.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,38 @@
 import React from "react";
-import { Link} from "react-router-dom";
+import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 import '../assets/styles/components/Heades.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
+import gravatar from '../utils/gravatar'
+import {object} from "prop-types";
 
-const Header = () => (
-    <header className="header">
-        <Link to="/">
-            <img className="header__img" src={logo} alt="Platzi Video" />
-        </Link>
-        <div className="header__menu">
-            <div className="header__menu--profile">
-                <img src={userIcon} alt="" />
-                <p>Perfil</p>
+const Header = props => {
+    const {user} = props;
+    const hasUser = Object.keys(user).length > 0;
+    return (
+        <header className="header">
+            <Link to="/">
+                <img className="header__img" src={logo} alt="Platzi Video"/>
+            </Link>
+            <div className="header__menu">
+                <div className="header__menu--profile">
+                    {hasUser ? <img src={gravatar(user.email)} alt={user.email}/> :
+                        <img src={userIcon} alt=""/>
+                    }
+                    <p>Perfil</p>
+                </div>
+                <ul>
+                    <li><a href="/">Cuenta</a></li>
+                    <li><Link to="/login">Iniciar Sesión</Link></li>
+                </ul>
             </div>
-            <ul>
-                <li><a href="/">Cuenta</a></li>
-                <li><Link to="/login">Iniciar Sesión</Link></li>
-            </ul>
-        </div>
-    </header>
-);
-
-export default Header;
+        </header>
+    )
+};
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+export default connect(mapStateToProps, null)(Header);
