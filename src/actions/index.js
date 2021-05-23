08d16f5a48265bde2d348a414af4fcc670078b9c
Ricardo Rodriguez Gonzalez <rrodriguez@agilistik.cl>
2021-05-23 19:10:59 -0400

    Curso 2: Clase 20

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,10 @@ export const deleteFavorite = payload => ({
     payload,
 });
 export const loginRequest = payload => ({
-   type: 'LOGIN_REQUEST',
-   payload
+    type: 'LOGIN_REQUEST',
+    payload
 });
+export const logoutRequest = payload => ({
+    type: 'LOGOUT_REQUEST',
+    payload
+})
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,15 @@ import '../assets/styles/components/Heades.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 import gravatar from '../utils/gravatar'
-import {object} from "prop-types";
+import {logoutRequest} from "../actions";
 
 const Header = props => {
     const {user} = props;
     const hasUser = Object.keys(user).length > 0;
+
+    const handleLogout = () => {
+        props.logoutRequest({})
+    }
     return (
         <header className="header">
             <Link to="/">
@@ -23,8 +27,16 @@ const Header = props => {
                     <p>Perfil</p>
                 </div>
                 <ul>
-                    <li><a href="/">Cuenta</a></li>
-                    <li><Link to="/login">Iniciar Sesión</Link></li>
+                    {hasUser ? <li><a href="/">{user.name}</a></li>
+                        : null
+                    }
+                    {hasUser ?
+                        <li><a href="#logout" onClick={handleLogout}>Cerrar Sesion</a></li>
+                        :
+                        <li>
+                            <Link to="/login">Iniciar Sesión</Link>
+                        </li>
+                    }
                 </ul>
             </div>
         </header>
@@ -35,4 +47,7 @@ const mapStateToProps = state => {
         user: state.user
     }
 }
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+    logoutRequest,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
